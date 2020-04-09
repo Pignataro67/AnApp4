c608d77904c247100c4eb98ffa63345dbfe2ca72
Pignataro67 <Pignataro67@gmail.com>
2020-04-09 06:08:27 -0700

    refractoring/spacing/separating reducers

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,22 +1,10 @@
 export default function rootReducer(state = {
-  isFetchingStartingLocation: false,
-  isFetchingDestination: false,
-  suggestedStartingLocations: [],
-  suggestedDestinations: [],
   isConvertingStartLatLong: false,
   isConvertingDestinationLatLong: false,
     }, action) {
   switch(action.type) {
     case 'FETCHING_MAPBOX_KEY':
       return {...state, isFetchingMapboxKey: true}
-    case "FETCHING_SUGGESTED_START_LOCATIONS":
-      return {...state, isFetchingStartingLocation: true, suggestedStartingLocations: []}
-    case "DISPLAY_START_LOCATIONS":
-      return {...state, isFetchingStartingLocation: false, suggestedStartingLocations: action.suggestedStartingLocations}
-    case "FETCHING_SUGGESTED_DESTINATIONS":
-      return {...state, isFetchingDestination: true, suggestedDestinations: []}
-    case "DISPLAY_DESTINATIONS":
-      return {...state, isFetchingDestination: false, suggestedDestinations: action.suggestedDestinations}
         default:
     return state
   }
