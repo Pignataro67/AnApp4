54bd608e3655acd2a0c6afb6f0e9d55dae23f549
Pignataro67 <Pignataro67@gmail.com>
2020-05-01 06:21:26 -0700

    removing unneeded code and comments, RSW

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
@@ -12,11 +12,9 @@ import 'semantic-ui-css/semantic.min.css';
 
 const middlewares = [thunk]
 
-if(process.env.NODE_ENV !== "production"){
+if(process.env.NODE_ENV !== "production") {
   middlewares.push(logger)
 }
-
-// ReactDOM.render(<App />, document.getElementById('root'));
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
@@ -29,4 +27,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
